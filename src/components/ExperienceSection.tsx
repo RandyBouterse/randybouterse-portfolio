53b0c9af675716728c5cd9598cfe90a207b4e41a
RandyBouterse <RR.Bouterse@Gmail.com>
2025-02-21 18:03:20 +0100

    Update ExperienceSection.tsx
    "import { Badge } from "@/components/ui/badge";"

--- src/components/ExperienceSection.tsx
+++ src/components/ExperienceSection.tsx
@@ -1,6 +1,7 @@
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Card, CardContent } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
 
 interface WorkExperience {
   company: string;
